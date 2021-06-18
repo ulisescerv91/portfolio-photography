9488d50efd63d6dd50fe38e43815ed12df261873
Ulises Cervantes <ulises.cerv@hotmail.com>
2021-06-18 14:50:25 -0500

    Section IMG

--- src/pages/Home/components/Sections/components/Card.jsx
+++ src/pages/Home/components/Sections/components/Card.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
 import  './Card.scss'
 export default function Card(props) {
     const {bgImg} = props;
     return (
         <div className='card' style={{ backgroundImage: `url(${bgImg})` }}>
-            card
+            <div className='card__shadow'></div>
+            <div className='card__link'>
+                Some text <span> <ArrowForwardIcon/> </span> 
+            </div>
         </div>
     )
 }
