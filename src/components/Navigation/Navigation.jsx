58b1842acaf533ad6b4ee0a0f58359960f18c221
Ulises Cervantes <ulises.cerv@hotmail.com>
2021-06-21 12:41:11 -0500

    NAV make iteration items

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,12 @@
 import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
 import './Navigation.scss';
+import CallMadeIcon from '@material-ui/icons/CallMade';
 
 export default function Navigation() {
     const [show,setShow] = useState(true);
+    const pages = ['Homepage', 'Portfolio', 'Motion']
+    const works = [ 'Comercial work', 'education', 'my presets', 'prints' ]
     return (
         <nav className='Navigation'>
             <div className='Navigation__shadow'></div>
@@ -17,44 +20,27 @@ export default function Navigation() {
                 <div className='Navigation__links___container'>
                 <div className="topFade"></div>
                 <div className="leftFade"></div>
-                    <ul className='Navigation__links___container__pages'>
-                        <li>
-                            <Link>
-                                Homepage
-                            </Link>
-                        </li>
-                        <li>
-                            <Link>
-                                Portfolio
-                            </Link>
-                        </li>
-                        <li>
-                            <Link>
-                                Motion
-                            </Link>
-                        </li>
+                    <ul className='Navigation__links___container__pages'>\
+                        {                            
+                            pages.map( (page,index) =>  
+                                <li key={index}>
+                                    <Link>
+                                        {page}
+                                    </Link>
+                                </li>                        
+                            )
+                        }
                     </ul>
-                    <ul className='Navigation__links___container__work'>
-                        <li>
-                            <Link>
-                                Comercial work
-                            </Link>
-                        </li>
-                        <li>
-                            <Link>
-                                education
-                            </Link>
-                        </li>
-                        <li>
-                            <Link>
-                                my presets
-                            </Link>
-                        </li>
-                        <li>
-                            <Link>
-                                print
-                            </Link>
-                        </li>
+                    <ul className='Navigation__links___container__work'>                        
+                        {
+                            works.map( (work, index) => 
+                                <li key={index}>
+                                    <Link>
+                                        {work} <CallMadeIcon/>
+                                    </Link>
+                                </li>
+                             )
+                        }
 
                     </ul>
                 </div>
